4981457856f2febbd4c67b16bf130ba46d464acf
Elena S <neverhood_2008@mail.ru>
2022-12-16 22:10:10 +0300

    Добавлено условие, что число не отрицительное

--- Example_8/Program.cs
+++ Example_8/Program.cs
@@ -1,14 +1,19 @@
 ﻿ //8. Напишите программу, которая на вход
 //принимает число (N), а на выходе показывает все чётные
 //числа от 1 до N.
-
 Console.Clear();
 Console.WriteLine("Введите число ");
 int number=int.Parse(Console.ReadLine()!);
 int temp=1;
+if (number>0)
+{
+Console.WriteLine("Четные числа (от 1 до исходного числа): ");
 while (temp<=number)
     {
         if (temp%2==0)
         Console.Write(temp+" ");
-    temp=temp+1;
-    }
+        temp=temp+1;
+    };
+}
+else 
+  Console.WriteLine("Введите положительное число");
